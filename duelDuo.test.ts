85cf425238003ac06117ce95604a78b7a1139112
Alex Lamb <alexlamb104@gmail.com>
2022-03-18 11:24:00 -0600

    Completed automated tests for duel duo

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -17,4 +17,22 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+
+    await driver.sleep(2000)
+})
+
+test('Clicking Draw displays id = "choices"', async () => {
+    let drawBtn = await driver.findElement(By.id('draw'))
+    await drawBtn.click()
+
+    await driver.sleep(3000)
+
+})
+
+test('Clicking add to duo displays robot in the div for player-duo', async () => {
+    let add = await driver.findElement(By.xpath("(//button)[2]"))
+
+    await add.click()
+
+    await driver.sleep(5000)
 })
